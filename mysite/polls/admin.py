3e00c75039a5324b242b8d1a72708bc61e393540
Arthur Barros <arthurpdb8@gmail.com>
2021-09-01 02:36:32 +0000

    Adiciona 3 models novos: Rotulo, Perfil e Autor e personaliza Admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,51 @@
+'''
 from django.contrib import admin
 from .models import *  #Choice, Question
 
 # Register your models here.
 admin.site.register(Choice)
 admin.site.register(Question)
+'''
+from django.contrib import admin
+from .models import Question, Choice, Rotulo, Autor, Perfil
+
+admin.AdminSite.site_header = 'Administração da Aplicação de Enquetes'
+### Definição do Formulário para as Perguntas
+#############################################
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['autor', 'question_text', 'rotulos']}),
+        ('Informações de Data', {'fields': ['pub_date', 'end_date']})
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'id', 'autor', 'pub_date', 'publicada_recentemente')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+
+
+### Definição do Formulário para os Rótulos
+###########################################
+class RotuloAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'id')
+
+admin.site.register(Rotulo, RotuloAdmin)
+
+
+### Definição do Formulário para os Autores
+###########################################
+class PerfilInline(admin.StackedInline):
+    model = Perfil
+
+class AutorAdmin(admin.ModelAdmin):
+    inlines = [PerfilInline]
+    list_display = ('nome', 'id')
+
+admin.site.register(Autor, AutorAdmin)
+### Registrando os módulos seguintes
+
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,22 +1,72 @@
+import datetime
 from django.db import models
 from django.utils import timezone
 
+
+###     Clase Autor
+##########################
+class Autor(models.Model):
+    nome = models.CharField(max_length=80)
+
+    class Meta:
+        ordering = ['nome']
+        verbose_name_plural = 'Autores'
+    def __str__(self):
+        return self.nome
+
+###     Classe Perfil
+###########################
+class Perfil(models.Model):
+    descricao = models.TextField()
+    cidade = models.CharField(max_length=50)
+    pais = models.CharField(max_length=30)
+    genero = models.CharField(max_length=100)
+    autor = models.OneToOneField(Autor, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'Informações de Perfil'
+    def __str__(self):
+        return self.autor.nome
+
+###     Classe Rotulo
+###########################
+class Rotulo(models.Model):
+    titulo = models.CharField(max_length=30)
+
+    class Meta:
+        ordering = ['titulo']
+        verbose_name = 'Rótulo'
+        verbose_name_plural = 'Rótulos'
+    def __str__(self):
+        return self.titulo
+
 # Create your models here.
 class Question(models.Model):
-	question_text = models.CharField(max_length=200)
-	pub_date = models.DateTimeField('Data de publicação')
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('Data de publicação')
+    end_date = models.DateField('Data de Encerramento', null=True)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE, null=True)
+    rotulos = models.ManyToManyField(Rotulo, verbose_name='Rótulos')
 
-	def __str__ (self):
-	    return self.question_text
+    def __str__ (self):
+        return self.question_text
 
-	def is_last(self):
+    def publicada_recentemente(self):
 	    date_now = timezone.now()
 	    return date_now-datetime.timedelta(days=1)<=self.pub_date<=date_now
+    publicada_recentemente.admin_order_field = 'data_publicacao'
+    publicada_recentemente.boolean = True
+    publicada_recentemente.short_description = 'É recente?'
+
 
 class Choice(models.Model):
-	question = models.ForeignKey(Question,on_delete=models.CASCADE)
-	choice_text = models.CharField(max_length=200)
-	votes = models.IntegerField(default=0)
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
 
-	def __str__ (self):
-	    return self.choice_text
+    class Meta:
+        ordering = ['choice_text']
+        verbose_name = 'Opção'
+        verbose_name_plural = 'Opções'
+    def __str__ (self):
+        return self.choice_text
